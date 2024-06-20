6331773352aca99a83fda6788390cb76b3d7d7e3
EstebanCastaño <c.pycasas@gmail.com>
2024-06-19 22:06:34 -0500

    Ejercicio inicial de back y fron con laravel y react

---
+++ src/components/ShowProducts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowProducts = () => {
+  return (
+    <div>Hola estoy en ShowProducts, donde puedo mostrar los productos</div>
+  )
+}
+
+export default ShowProducts
